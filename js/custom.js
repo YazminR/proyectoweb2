b7e7cb21168ec024835d9f382c62dd4e64d6d0e4
Yazmin Ruiz <yazmiinruiiz@gmail.com>
2016-05-13 13:48:10 -0600

    Modificacion 13/05/16

--- js/custom.js
+++ js/custom.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
   var document = window.document;
 
   function mainSlider() {
-    $('.bxslider').bxSlider({
+  $('.bxslider').bxSlider({
       pagerCustom: '#bx-pager',
       mode: 'fade',
       nextText: '',
@@ -43,17 +43,17 @@ $(document).ready(function() {
     });
   }
 
-  mainSlider();
+mainSlider();
 
-  var $links = $(".bx-wrapper .bx-controls-direction a, #bx-pager a");
-  $links.click(function(){
-     $(".slider-caption").removeClass('animated fadeInLeft');
-     $(".slider-caption").addClass('animated fadeInLeft');
-  });
+//  var $links = $(".bx-wrapper .bx-controls-direction a, #bx-pager a");
+  //$links.click(function(){
+     //$(".slider-caption").removeClass('animated fadeInLeft');
+     //$(".slider-caption").addClass('animated fadeInLeft');
+  //});
 
-  $(".bx-controls").addClass('container');
-  $(".bx-next").addClass('fa fa-angle-right');
-  $(".bx-prev").addClass('fa fa-angle-left');
+  //$(".bx-controls").addClass('container');
+  //$(".bx-next").addClass('fa fa-angle-right');
+ // $(".bx-prev").addClass('fa fa-angle-left');
 
 
   })(window, jQuery);
